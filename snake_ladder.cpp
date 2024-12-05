52309adb0f97a854ad8923eb651a6eee04128761
Dhanapoti-Roy <bsse1510@iit.du.ac.bd>
2024-12-05 13:49:49 +0600

    Add files via upload

---
+++ snake_ladder.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Player {
+public:
+    int position;
+    string name;
+    Player(string name);
+    int getPosition();
+};
+
+Player::Player(string name) {
+    this->name = name;
+    this->position = 0;
+}
+
+int Player::getPosition() {
+    return position;
+}
+
+class SnakesladdersLink {
+public:
+    int square;
+    int endSquare;
+    SnakesladdersLink* next;
+    SnakesladdersLink* previous;
+    SnakesladdersLink(int square,int endSquare);
+    void displaySnakeLink();
+};
+
+SnakesladdersLink::SnakesladdersLink(int square,int endSquare) {
+    this->square = square;
+    this->endSquare = endSquare;
+    next = NULL;
+    previous = NULL;
+}
+class Board {
+private:
+    SnakesladdersLink* first;
+    SnakesladdersLink* last;
+public:
+    Board();
+    void insertSquare(int square,int endSquare);
+    void displaySquare();
+    void handleInsertSquare(int index);
+    void displaybord();
+    void rules();
+    SnakesladdersLink* findSquare(int Squareid);
+    int snakesarray[7][2] = {
+        {30, 7},
+        {47, 15},
+        {56, 19},
+        {73, 51},
+        {82, 42},
+        {92, 75},
+        {98, 55}
+    };
+    int laddersarray[6][2] = {
+        {4, 25},
+        {21, 39},
+        {29, 74},
+        {43, 76},
+        {63, 80},
+        {71, 89}
+    };
+
+};
+
+void SnakesladdersLink::displaySnakeLink() {
+    cout << square <<" "<<endSquare<< endl;
+}
+
+SnakesladdersLink* Board::findSquare(int Squareid){
+    SnakesladdersLink* findlink = first;
+    while(findlink != NULL){
+        if(findlink->square==Squareid){
+            return findlink;
+        } else{
+            findlink=findlink->next;
+        }
+    }
+    return NULL;
+}
